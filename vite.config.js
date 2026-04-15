6626c74c8d96b980cadda36c2c72a11b1cec5482
LeoDumas <leo.ndumas980@gmail.com>
2026-04-15 17:10:58 +0200

    FEAT: Setup Vitest, CI/CD, QuoteBox test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom", // Tells Vitest to run in a browser-like environment
+    setupFiles: "./setupTests.js", // We will create this file next
+    globals: true,
+  },
 });
