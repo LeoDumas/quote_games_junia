5396166fe2991874c5a0a9e2ae76da3b0a48a3ec
LeoDumas <leo.ndumas980@gmail.com>
2026-04-15 14:04:08 +0200

    FEAT: basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import NotFound from './pages/NotFound/NotFound'
-import './App.css'
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <NotFound/>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
