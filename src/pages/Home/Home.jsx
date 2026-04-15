1ecc03bafae59f971d04ad0348a275a1e7519edd
LeoDumas <leo.ndumas980@gmail.com>
2026-04-15 16:53:31 +0200

    FEAT: Quote box element

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
+import QuoteBox from "../../components/QuoteBox/QuoteBox";
+
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>HOME</h1>
+      <QuoteBox
+        text="This is a long ahh quote to test the element, and this is not really good"
+        author="Fan De Gameplay"
+      />
     </div>
   );
 }
